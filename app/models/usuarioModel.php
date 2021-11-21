ae7d31f29e8bbf512aa87e3022348fc564ce242b
Jonathan Cruz <cruzjonathan.e@gmail.com>
2021-11-21 14:46:40 -0300

    Usuario + Registro.

---
+++ app/models/usuarioModel.php
@@ -0,0 +1,37 @@
+<?php
+    require_once('app/core/ConexionDB.php');
+
+    class UsuarioModelo extends ConexionPDO {
+        public $usuarioId;
+        public $nombre;
+        public $apellido;
+        public $email;
+        public $clave;
+        public $activo;
+
+        public function guardar(){
+            $this->query = "INSERT INTO usuarios (Nombre, Apellido, Email, Clave, Activo)
+                            VALUES(:nombre, :apellido, :email, :clave, :activo)";
+            $this->ejecutar(array(
+                ':nombre' => $this->nombre,
+                ':apellido' => $this->apellido,
+                ':email' => $this->email,
+                ':clave' => $this->clave,
+                ':activo' => $this->activo
+            ));
+            return $this->rows;
+        }
+
+
+        public function verificarEmail(){
+            $this->query = "SELECT U.Email 
+                            FROM usuarios U
+                            WHERE U.Email = :email";
+            $this->obtenerRows(array(
+                ':email' => $this->email
+            ));
+            return $this->rows;
+        }
+    }
+
+?>
---
+++ registro.php
@@ -0,0 +1,58 @@
+<?php
+    require_once './app/core/config.php';
+    require_once './app/core/auxiliares.php';
+    require_once './app/models/usuarioModel.php';
+
+    $display = "none";
+    if ( isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['email']) &&
+        isset($_POST['password-1']) && isset($_POST['password-2']) ){
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $email = $_POST['email'];
+        $password1 = $_POST['password-1'];
+        $password2 = $_POST['password-2'];
+
+        if ( $password1 == $password2 ){
+            $usuario = new UsuarioModelo();
+
+            $usuario->nombre = $nombre;
+            $usuario->apellido = $apellido;
+            $usuario->email = $email;
+
+            $existe = $usuario->verificarEmail();
+
+            if ( count($existe) > 0 ){
+                $display = "block";
+                $mensaje = $usuario->estado == 'Conectado' ? 'El email ya está registrado' : 'Error al registrarse';
+                require ('app/views/registro.html');
+                return;
+            } 
+
+            $usuario->clave = generarHash($password1);
+            $usuario->activo = 1;
+
+            $usuario->guardar();
+
+
+            if ( $usuario->estado == 'Conectado' ){
+                //header('Location: '.URL.'/login.php');
+                $mensaje =  'Registro correcto';
+                require ('app/views/registroCorrecto.html');
+
+            } else {
+                $display = "block";
+                $mensaje =  'Error al registrarse ';
+                require ('app/views/registro.html');
+            }
+        } else {
+            $display = "block";
+            $mensaje = 'Las contraseñas no coinciden';
+            require ('app/views/registro.html');
+        }
+
+    } else {
+        $mensaje = '';
+        require ('app/views/registro.html');
+    }
+
+?>
